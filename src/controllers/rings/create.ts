9440ebe43dc0b3b6c0bd6ca39fdf7aedc6f3b649
Raul Santana <ti.raul77@gmail.com>
2024-09-17 17:26:08 -0300

    fix: treatment of errors to create ring controller

--- src/controllers/rings/create.ts
+++ src/controllers/rings/create.ts
@@ -2,6 +2,8 @@ import { Forgers } from '@/database/entities/Ring'
 import { makeCreateRingUseCase } from '@/use-cases/rings/factories/make-create-ring-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { MaxRingsForgedBySpecie } from '@/use-cases/errors/max-rings-forged-by-this-species-error'
+import { BearerNotFoundError } from '@/use-cases/errors/bearer-not-found-error'
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
   const createRingBodySchema = z.object({
@@ -30,15 +32,37 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
 
   const createRingUseCase = makeCreateRingUseCase()
 
-  await createRingUseCase.execute({
-    name,
-    power,
-    forgedBy,
-    imageUrl,
-    bearerId,
-    startDate,
-    endDate,
-  })
+  try {
+    await createRingUseCase.execute({
+      name,
+      power,
+      forgedBy,
+      imageUrl,
+      bearerId,
+      startDate,
+      endDate,
+    })
 
-  return reply.status(201).send()
+    return reply.status(201).send()
+  } catch (error) {
+    if (error instanceof MaxRingsForgedBySpecie) {
+      return reply.status(400).send({
+        errorCode: 'MaxRingsForgedBySpecie',
+        message:
+          'O número máximo de anéis forjados por essa spécie já foi atingido.',
+      })
+    }
+
+    if (error instanceof BearerNotFoundError) {
+      return reply.status(404).send({
+        errorCode: 'BearerNotFoundError',
+        message: 'Portador não encontrado.',
+      })
+    }
+
+    // Handle other unknown errors
+    return reply.status(500).send({
+      message: 'Internal server error.',
+    })
+  }
 }
